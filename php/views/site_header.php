16211801608298ec512a451ebc402279172b7aaf
Joonas Koivunen <joonas.koivunen@jmkits.fi>
2012-01-25 17:55:52 +0200

    site_header.php: list all mappings for navigation

--- php/views/site_header.php
+++ php/views/site_header.php
@@ -4,6 +4,12 @@ if (!isset($model)) {
     die('make an array named $model and add an mapping for "title"');
 }
 
+global $CFG;
+
+function echo_link($uri_part, $text='') {
+    echo '<a href="' . link_to_url($uri_part) . '">' . (strlen($text) > 0 ? $text : $uri_part) . '</a>';
+}
+
 ?>
 
 <!doctype html>
@@ -15,6 +21,14 @@ if (!isset($model)) {
 	
 	<h1><?php echo $model['title']; ?></h1>
 	
+	<div class="navi">
+		<ul>
+		<?php foreach ($CFG['mappings'] as $uri_part => $handler) { ?>
+			<li><?php echo_link($uri_part); ?></li>
+		<?php } ?>
+		</ul>
+	</div>
+
 	<div class="main">
 	
 	<?php /* view html from here */
