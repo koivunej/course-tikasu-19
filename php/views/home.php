3ed9b671462e39015f20fd055ccb180c33251477
Joonas Koivunen <joonas.koivunen@jmkits.fi>
2012-01-25 12:07:14 +0200

    home.php: some session diagnostics

--- php/views/home.php
+++ php/views/home.php
@@ -3,4 +3,18 @@
 $model = array('title' => 'home');
 
 render_template_begin($model);
+
+?>
+	
+<dl>
+	<dt>authenticated</dt>
+	<dd><?php $auth = UserDetailsContext::isAuthenticated(); echo ($auth == TRUE ? "yes" : "no"); ?></dd>
+	<dt>username</dt>
+	<dd><?php $username = UserDetailsContext::getUsernameOrNull(); echo ($username == NULL ? "not authenticated" : $username); ?></dd>
+</dl>
+
+<pre><?echo var_dump($_SESSION); ?></pre>
+
+<?php
+
 render_template_end($model);
