f6021a71546d86b4e19f09cd1c35092ff4644559
Joonas Koivunen <joonas.koivunen@jmkits.fi>
2012-01-29 19:13:31 +0200

    index.php: typo in mappings

--- php/index.php
+++ php/index.php
@@ -19,7 +19,7 @@ $CFG["mappings"]["/invoices"] = array('handler' => "@/invoices/list", 'name' => 
 $CFG["mappings"]["/invoices/list"] = array('handler' => "invoices/list.php", 'name' => 'List all invoices', 'hidden' => 'UserDetailsContext::isNotAuthenticated');
 $CFG["mappings"]["/invoices/add"] = array('handler' => "invoices/edit.php", 'name' => 'Add an invoice', 'hidden' => 'UserDetailsContext::isNotAuthenticated');
 $CFG["mappings"]["/invoices/view"] = array('handler' => 'invoices/view.php', 'hidden' => TRUE);
-$CFG["mappings"]["/invoices/edit"] = array('handler' => 'invioces/edit.php', 'hidden' => TRUE);
+$CFG["mappings"]["/invoices/edit"] = array('handler' => 'invoices/edit.php', 'hidden' => TRUE);
 
 $CFG['site'] = 'http://aapiskukkowww.cs.tut.fi:8080/tikaja/' . basename(dirname(__FILE__));
 
