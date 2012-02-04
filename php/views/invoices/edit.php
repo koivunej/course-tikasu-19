fa95e2dec33a3195f3bc776f10a954eef603d7bb
Oiva Moisio <oiva.moisio@tut.fi>
2012-02-04 20:36:32 +0200

    edit on melkeen valmis muutaman syntaxi virheen/bugi korjauksen jalkeen

--- php/views/invoices/edit.php
+++ php/views/invoices/edit.php
@@ -24,6 +24,7 @@ class Invoice {
 }
 
 function handle_post($model, $context) {
+    $model["obj"] = new Invoice();
     // service discovery through $context
     // service call
     // redirection to view page -- redirect_and_exit("/invoices/view?id=" . $inserted_id)
@@ -41,10 +42,27 @@ $model = array("title" => "add an invoice");
 $model["obj"] = new Invoice();
 
 // we are creating new if the id is NULL, otherwise we are editing an old
-$is_editing = FALSE; // $model["obj"]->id == NULL;
+$is_editing = FALSE; // 
+$model["obj"]->cam_id = NULL;
 
 if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
     $model = handle_post($model, $context);
+    if ($_POST["sent"] == "high") {
+	//only one value come from high campaign id
+	$model["obj"]->cam_id = $_POST["cam_id"];
+    }
+    
+    else if ($_POST["sent"] == "low") {
+	//now we can open up db conncetion
+	$conn_id = $context->db;
+	
+	//making insert
+	$insert = "INSERT INTO invoices (id, due_at, reference_number, late_fee, sent, campaign_id)
+	  VALUES (invoices_id_seq.NEXTVAL,".$_POST["due_at"].",".$_POST["ref_number"].",'0','F',".$_POST["cam_id"].")";
+	
+	$conn_id->beginTransaction ();
+	$conn_id->query(insert);
+    }
 }
 
 if ( $_SERVER["REQUEST_METHOD"] == "GET") {
@@ -58,39 +76,62 @@ render_template_begin($model);
 
 if (!$is_editing) {
 
-    echo "<form method=\"post\">";
-    echo "<table border = \"1\">";
-	echo "<thead>";
-		echo "<tr>";
-			echo "<td> Name </td>";
-			echo "<td> Start date </td>";
-			echo "<td> End date </td>";
-		echo "</tr>";
-	echo "</thead>";
+    if ($model["obj"]->cam_id == NULL) {
+	echo "<form method=\"post\">";
+	  echo "<table border = \"1\">";
+	  echo "<thead>";
+	    echo "<tr>";
+	      echo "<td> Name </td>";
+	      echo "<td> Start date </td>";
+	      echo "<td> End date </td>";
+	    echo "</tr>";
+	  echo "</thead>";
 
-    //opening connection to database                                                                                                                                                                                                                                                                            
+	//opening connection to database                                                                                                                                                                                                                                                                            
     
-    $conn_id = $context->db;
+	$conn_id = $context->db;
 
-    $query = "SELECT DISTINCT name, starts_at, ends_at FROM campaigns, invoices WHERE campaigns.id NOT IN 
-	   (SELECT campaigns.id FROM invoices, campaigns WHERE (campaigns.id = campaign_id)
-	    OR campaigns.active = 'T')";
+	$query = "SELECT DISTINCT campaigns.id,name, starts_at, ends_at FROM campaigns, invoices WHERE campaigns.id NOT IN 
+		   (SELECT campaigns.id FROM invoices, campaigns WHERE (campaigns.id = campaign_id)
+		    OR campaigns.active = 'T')";
 	
-    $conn_id->beginTransaction();
+	$conn_id->beginTransaction();
+	
+	//gerring necessary rows                                                                                                                                      
+	$rows = $conn_id->query ($query);
 
-    //gerring necessary rows                                                                                                                                      
-    $rows = $conn_id->query ($query);
-
-    foreach ($rows as $iter) {
 	echo "<tbody>";
-	echo "<tr>";
-	echo "<td>".$iter["name"]."</td>";
-	echo "<td>".$iter["starts_at"]."</td>";
-	echo "<td>".$iter["ends_at"]."</td>";
-	echo "<td><input type=\"submit\" value=\"Create\"></td>";
-	echo "</tr>";
-    }	
-   echo "</form>";	
+	foreach ($rows as $iter) {
+	      echo "<tr>";
+	        echo "<td>".$iter["name"]."</td>";
+	        echo "<td>".$iter["starts_at"]."</td>";
+	        echo "<td>".$iter["ends_at"]."</td>";     
+	        echo "<td><input type=\"submit\" value=\"Create\"></td>";
+	      echo "</tr>";
+	}	
+	echo "</tbody>";
+	echo "</table>";
+	echo "<input type=\"hidden\" value=\"".$iter["id"]."\" name=\"cam_id\">";                                                                     
+	//fake send for post function                                                                                                                 
+	echo "<input type=\"hidden\" value=\"high\" name=\"sent\">";  
+	echo "</form>";
+    }
+    
+    else {
+	echo "<form method=\"post\">";
+	echo "Due date (yyyy-mm-dd): <input type=\"text\" value=\"".$model["obj"]->due_at."\" method=\"post\" name=\"due_date\" ><br>";
+	echo "Reference number (at least 5 letters): <input type=\"text\" value=\"".$model["obj"]->ref_number."\" method=\"post\" name=\"ref_number\"><br>";
+	echo "Late fee: ".$model["obj"]->late_fee."<br>";
+	echo "Sent: F<br>";
+	//lets send fake value because there is stuff to consider ^^
+	echo "<input type=\"hidden\" value=\"low\" name=\"sent\">";
+	echo "Campaign number: ".$model["obj"]->cam_id."<br>";
+	echo "<input type=\"hidden\" value=\"".$model["obj"]->cam_id."\" name=\"cam_id\">";
+	echo "<input type=\"submit\" value=\"Save\">";
+	echo "</form>";
+											    
+    }
+	
 }
 
 else {
@@ -105,20 +146,23 @@ else {
     
     echo "<form method = \"post\">";
     foreach ($row as $iter) {
+	echo "<table>";
 	echo "<tr><td> Id: </td>";
 	echo "<td>".$iter["id"]."</td></tr><br>";
 	echo "<tr><td> Due date: </td>";
 	echo "<td><input type=\"text\" value = \"".$iter["due_at"]."\"</td></tr><br>";
 	echo "<tr><td> Reference number: </td>";
-	echo "<td>".$iter["reference_number"]."</td></tr><br>";
+	echo "<td><input type=\"text\" value = \"".$iter["reference_number"]."\"</td></tr><br>";
 	echo "<tr><td> Late fee: </td>";
-	echo "<td><input type=\"text\" value = \"".$iter["late_fee"]."\"</td></tr><br>";
+	echo "<td>".$iter["late_fee"]."</td></tr><br>";
 	echo "<tr><td> Sent: </td>";
 	echo "<td>".$iter["sent"]."</td></tr><br>";
 	echo "<tr><td> Campaign number: </td>";
 	echo "<td>".$iter["campaign_id"]."</td></tr><br>";
 	echo "<tr><td> Previous invoice: </td>";
 	echo "<td>".$iter["previous_invoice_id"]."</td></tr><br>";
+	echo "</table>";
+	echo "<input type=\"submit\" value=\"Save\">";
     }
     echo "</form>";
 }
