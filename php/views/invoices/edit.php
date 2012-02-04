0132ac17c582dc7be0327b02371d5e994f6784df
Oiva Moisio <oiva.moisio@tut.fi>
2012-02-04 16:12:06 +0200

    editin valipalautus :)... list.php varmuudenvuoksi kun ei muista muuttiko siita mitaan

--- php/views/invoices/edit.php
+++ php/views/invoices/edit.php
@@ -4,14 +4,22 @@
  * view for /invoices/add -- add a new invoice
  */
 
-function handle_post($model, $context) {
+//Invoice class
+
+class Invoice {
     
+}
+
+function handle_post($model, $context) {
+    $is_editing = TRUE;
     // service discovery through $context
     // service call
     // redirection to view page -- redirect_and_exit("/invoices/view?id=" . $inserted_id)
     // exception handling (in case of invalid data): 
     
 }
+
+global $context;
 
 $model = array("title" => "add an invoice");
 
@@ -28,44 +36,61 @@ if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
 }
 
 render_template_begin($model);
-?>
 	
-<form method="post">
-<table border = "1">
-	<thead>
-		<tr>
-			<td> Name </td>
-			<td> Start date </td>
-			<td> End date </td>
-		</tr>
-	</thead>
-<?php
+if (!is_editing) {
 
-//opening connection to database                                                                                                                              
-global $context;                                                                                                                                              
-                                                                                                                                                              
-$conn_id = $context->db;
+    echo "<form method=\"post\">";
+    echo "<table border = \"1\">";
+	echo "<thead>";
+		echo "<tr>";
+			echo "<td> Name </td>";
+			echo "<td> Start date </td>";
+			echo "<td> End date </td>";
+		echo "</tr>";
+	echo "</thead>";
 
-$query = "SELECT DISTINCT name, starts_at, ends_at FROM campaigns, invoices WHERE campaigns.id NOT IN 
+    //opening connection to database                                                                                                                                                                                                                                                                            
+    
+    $conn_id = $context->db;
+
+    $query = "SELECT DISTINCT name, starts_at, ends_at FROM campaigns, invoices WHERE campaigns.id NOT IN 
 	   (SELECT campaigns.id FROM invoices, campaigns WHERE (campaigns.id = campaign_id)
-  OR campaigns.active = 'T')";
+	    OR campaigns.active = 'T')";
 	
-$conn_id->beginTransaction();
+    $conn_id->beginTransaction();
 
-//gerring necessary rows                                                                                                                                      
-$rows = $conn_id->query ($query);
+    //gerring necessary rows                                                                                                                                      
+    $rows = $conn_id->query ($query);
 
-foreach ($rows as $iter) {
-    echo "<tbody>";
-    echo "<tr>";
-    echo "<td>".$iter["name"]."</td>";
-    echo "<td>".$iter["starts_at"]."</td>";
-    echo "<td>".$iter["ends_at"]."</td>";
-    echo "<td><input type=\"submit\" value=\"Create\"></td>";
-    echo "</tr>";
+    foreach ($rows as $iter) {
+	echo "<tbody>";
+	echo "<tr>";
+	echo "<td>".$iter["name"]."</td>";
+	echo "<td>".$iter["starts_at"]."</td>";
+	echo "<td>".$iter["ends_at"]."</td>";
+	echo "<td><input type=\"submit\" value=\"Create\"></td>";
+	echo "</tr>";
+    }	
+   echo "</form>";	
 }
+
+else {
+    //connection to database
+    $conn_id = $context->db;
+    
+    $query = "SELECT DISTINCT * FROM campaigns WHERE id = ".model[obj]->id;
+    
+    $conn_id->beginTransaction();
+    
+    $row = $conn_id->query ($query);
+    
+    echo "<form method = \"post\">";
+    echo "<tr><td> Id: </td>";
+    echo "<td>".$row[id]."<td></tr>";
+    echo "</form>";
+}
+
 ?>
-</form>
       
 	
 <?php
