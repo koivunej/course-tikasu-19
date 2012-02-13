00b0d99efc1ce67d6e86121f8836d7e17839672c
Joonas Koivunen <joonas.koivunen@jmkits.fi>
2012-02-13 02:39:56 +0200

    InvoiceService: stubs

--- php/libs/InvoiceService.php
+++ php/libs/InvoiceService.php
@@ -2,22 +2,38 @@
 
 class InvoiceService {
  
-    var $context;
-    
-    function __construct($context) {
-	$this->context = $context;
-    }
-    
-    function getById($id) {
-	$db = $context->db;
+	var $context;
 	
-	$tx = $db->beginTransaction();
-	$paluu = $db->query('SELECT * FROM users WHERE username = ?', array($username);
-	return $paluu;
-	$tx->commit();
-	// transaktiot jne.
-    }
-    
+	function __construct($context) {
+		$this->context = $context;
+	}
+	
+	function getById($id) {
+		$db = $context->db;
+		
+		$tx = $db->beginTransaction();
+		$results = $db->queryAtMostOneResult('SELECT * FROM invoices WHERE id = ?', array($id);
+		$tx->commit();
+		
+		$ret = new Invoice();
+		$db->hydrate($ret, $results, array("sum"));
+		
+		return $ret;
+	}
+
+	function saveOrUpdate($invoice) {
+		
+	}
+	
+	private function save($invoice) {
+		
+	}
+	
+	private function update($invoice) {
+		
+	}
+
+	
     /**
      * factory method called by when Context->invoiceService
      * is accessed for the first time; rigged at index.php
