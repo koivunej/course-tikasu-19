da1393b4d911d538acea00418032c972da8a7f42
Joonas Koivunen <joonas.koivunen@jmkits.fi>
2012-02-13 04:02:15 +0200

    Invoice: fixed default values

---
+++ php/libs/Campaign.php
@@ -0,0 +1,14 @@
+<?php
+
+class Campaign {
+	public $id;
+	public $name;
+	public $starts_at;
+	public $ends_at;
+	public $budget;
+	public $price_per_second;
+	public $active;
+	public $user_id;
+	public $contactperson_id;
+	public $adv_vat;
+}
--- php/libs/Invoice.php
+++ php/libs/Invoice.php
@@ -1,13 +1,7 @@
 <?php
 
 class Invoice {
-	function __construct() {    
-		$due_at = date("Y-m-d", time() + 3*7*24*60*60*1000);
-		$ref_number = "00000";
-		$late_fee = 0;
-		$sent = "F";
-	}
-	
+
 	public $id;
 	public $due_at;
 	public $reference_number;
@@ -16,5 +10,12 @@ class Invoice {
 	public $campaign_id;
 	public $previous_invoice_id;
 	public $sum; // transient, calculated
+
+	function __construct() {    
+		$this->due_at = date("Y-m-d", time() + 3*7*24*60*60);
+		$this->reference_number = "00000";
+		$this->late_fee = 0;
+		$this->sent = "F";
+	}
 
 }
