884931a53732f122d72ad62e0fd8cd1cd34048de
Joonas Koivunen <joonas.koivunen@jmkits.fi>
2012-02-20 03:32:34 +0200

    home: drop the debug info

--- php/views/home.php
+++ php/views/home.php
@@ -4,20 +4,21 @@ $model = array('title' => 'home');
 
 render_template_begin($model);
 
+
+
+$username = UserDetailsContext::getUsernameOrNull();
+if ($username !== NULL) {
 ?>
 
-<h2>authentication state</h2>	
-<dl>
-	<dt>authenticated</dt>
-	<dd><?php $auth = UserDetailsContext::isAuthenticated(); echo ($auth == TRUE ? "yes" : "no"); ?></dd>
-	<dt>username</dt>
-	<dd><?php $username = UserDetailsContext::getUsernameOrNull(); echo ($username == NULL ? "not authenticated" : $username); ?></dd>
-</dl>
-
-	
-<h2>session</h2>
-<pre><?echo var_dump($_SESSION); ?></pre>
+<p>Welcome <?php echo $username; ?>
 
 <?php
+} else {
+?>
+
+<p>Welcome! Please <?php echo_link('/login', 'log in');?>!</p>
+
+<?php
+}
 
 render_template_end($model);
